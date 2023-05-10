83d394febc8618dcd378def5ca7b2d38125bbb80
Ayom04 <abdullahayomide04@gmail.com>
2023-05-10 17:59:16 +0100

    working with the engine

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -69,3 +69,126 @@ class test_amenity(test_basemodel):
                          "Test Amenity2")
         amenity.delete()
         amenity2.delete()
+
+    def test_amenity_create(self):
+        """test case for amenity create"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.name, "Test Amenity")
+        amenity.delete()
+
+    def test_amenity_update(self):
+        """test case for amenity update"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        amenity.name = "Test Amenity Updated"
+        amenity.save()
+        self.assertEqual(amenity.name, "Test Amenity Updated")
+        amenity.delete()
+
+    def test_amenity_delete(self):
+        """ test case for amenity delete"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        amenity.delete()
+        self.assertEqual(len(Amenity.objects.all()), 0)
+        amenity.delete()
+
+    def test_amenity_name(self):
+        """ test case for amenity name"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.name, "Test Amenity")
+        amenity.delete()
+
+    def test_amenity_value(self):
+        """ test case for amenity value"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.value, Amenity)
+        amenity.delete()
+
+    def test_amenity_name_string(self):
+        """ test case for amenity name string"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.name, "Test Amenity")
+        amenity.delete()
+
+    def test_amenity_value_string(self):
+        """ test case for amenity value string"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.value, Amenity)
+        amenity.delete()
+
+    def test_amenity_name_dict(self):
+        """ test case for amenity name dict"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.name, "Test Amenity")
+        amenity.delete()
+
+    def test_amenity_value_dict(self):
+        """ test case for amenity value dict"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.value, Amenity)
+        amenity.delete()
+
+    def test_amenity_name_list(self):
+        """ test case for amenity name list"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.name, "Test Amenity")
+        amenity.delete()
+
+    def test_amenity_value_list(self):
+        """ test case for amenity value list"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.value, Amenity)
+        amenity.delete()
+
+    def test_amenity_name_none(self):
+        """ test case for amenity name none"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.name, "Test Amenity")
+        amenity.delete()
+
+    def test_amenity_value_none(self):
+        """ test case for amenity value none"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.value, Amenity)
+        amenity.delete()
+
+    def test_amenity_name_empty(self):
+        """ test case for amenity name empty"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.name, "Test Amenity")
+        amenity.delete()
+
+    def test_amenity_value_empty(self):
+        """ test case for amenity value empty"""
+        amenity = Amenity()
+        amenity.name = "Test Amenity"
+        amenity.save()
+        self.assertEqual(amenity.value, Amenity)
+        amenity.delete()
