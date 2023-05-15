800f94d4d3f1feb8e72e6588d32964b1f28fe389
Ayom04 <abdullahayomide04@gmail.com>
2023-05-15 19:38:27 +0100

    working on dependencies

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,8 +3,18 @@
 Contains the class TestConsoleDocs
 """
 
+from unittest.mock import patch
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review 
+from models.state import State
 import console
 import inspect
+import io
 import pep8
 import unittest
 HBNBCommand = console.HBNBCommand
@@ -39,3 +49,11 @@ class TestConsoleDocs(unittest.TestCase):
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
+
+    def test_unknown(self):
+        """ Command that does not exist """
+        msg = "*** Unknown syntax: asd\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("asd")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
