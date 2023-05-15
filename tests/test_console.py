1f9b86f451bf7ff1403d55a97933a16413e7c57e
Ayom04 <abdullahayomide04@gmail.com>
2023-05-15 20:38:12 +0100

    test count class

--- tests/test_console.py
+++ tests/test_console.py
@@ -6,11 +6,12 @@ Contains the class TestConsoleDocs
 from unittest.mock import patch
 from datetime import datetime
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 from models.user import User
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from models.review import Review 
+from models.review import Review
 from models.state import State
 import console
 import inspect
@@ -55,5 +56,96 @@ class TestConsoleDocs(unittest.TestCase):
         msg = "*** Unknown syntax: asd\n"
         with patch('sys.stdout', new=io.StringIO()) as f:
             HBNBCommand().onecmd("asd")
+            _st = f.getvalue()
+            self.assertEqual(msg, _st)
+
+    def setUp(self):
+        """ Set up for all methods """
+        try:
+            remove("file.json")
+        except Exception:
+            pass
+        FileStorage._FileStorage__objects = {}
+
+    def tearDown(self):
+        """ Tear down for all methods """
+        try:
+            remove("file.json")
+        except Exception:
+            pass
+
+    def test_help_help(self):
+        """  Test for help of quit command """
+        msg = "List available commands with \"help\" or " \
+              "detailed help with \"help cmd\".\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("help help")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
+
+    def test_help_quit(self):
+        """  Test for help of quit command """
+        msg = "Quit command to exit the program\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("help quit")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
+
+    def test_help_EOF(self):
+        """  Test for help of EOF command """
+        msg = "EOF command to exit the program\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("help EOF")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
+
+    def test_help_create(self):
+        """  Test for help of create command """
+        msg = "Create an instance if the Model exists\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("help create")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
+
+    def test_help_show(self):
+        """  Test for help of show command """
+        msg = "Print dict of a instance in base of it's ID\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("help show")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
+
+    def test_help_destroy(self):
+        """  Test for help of destroy command """
+        msg = "Deletes an instance based on it's ID and save the changes\n \
+        Usage: destroy <class name> <id>\n"
+
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("help destroy")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
+
+    def test_help_all(self):
+        """  Test for help of all command """
+        msg = "Print all the instances saved in file.json\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("help all")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
+
+    def test_help_update(self):
+        """  Test for help of update command """
+        msg = "Usage: update <class name> <id> <attribute name> " \
+              "<attribute value>\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("help update")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
+
+    def test_help_count(self):
+        """  Test for help of count command """
+        msg = "Usage: count <class name> or <class name>.count()\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("help count")
             st = f.getvalue()
             self.assertEqual(msg, st)
